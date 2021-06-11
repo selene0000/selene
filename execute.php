302d20529061007f61814cdee4accfca31fa146f
selene0000 <84371524+selene0000@users.noreply.github.com>
2021-06-11 10:26:07 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -47,7 +47,16 @@
 		header("Content-Type: application/json");
 		//i parametri sono cosa voglio mandare indietro al mio utente
 		$parameters = array('chat_id' => $chatId, "text" => $text);
-		
+		if($text=="ciao" || $text=="/ciao"){
+      $text = "Benvenuto sulla pagina del bot dell'8 marzo";
+      $parameters = array('chat_id' => $chatId, "text"=> $text);
+    }
+  if($text == "foto"){
+    sendFoto($chatId, "foto.ipg", false, "la mia foto", $api);
+  }
+if($text == 'barz'|| $text=='/barz'){
+$barze[0] = "che cos'è una zebra? un cavallo evaso dal carcere"; 
+$barze[1] =  "colmo truffatore?
 		
 		
 		
